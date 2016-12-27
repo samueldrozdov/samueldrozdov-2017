017353453d7177699c94970e3c9399929a460647
sdrozzy <sdrozzy@gmail.com>
2016-12-27 14:53:11 -0500

    Change navbar to flexbox. Add navbar animations

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,23 @@
-console.log("test");
+$(document).ready(function() {
+  $(".profile-pic").click(function(e) {
+    console.log("click");
+    $('html, body').animate({ scrollTop: 0 }, 'medium');
+  });
 
-$(window).click(function() {
-  console.log("click");
-})
+  $(window).scroll(function() {
+    var height = $(window).scrollTop();
+    updateHeader(height);
+  });
+});
+
+var updateHeader = function(pos) {
+  if(pos > 30) {
+    $(".nav-item").addClass("nav-item-scrolled");
+    $(".profile-pic").addClass("profile-pic-scrolled");
+    $(".nav-bar").addClass("nav-bar-scrolled");
+  } else {
+    $(".nav-item").removeClass("nav-item-scrolled");
+    $(".profile-pic").removeClass("profile-pic-scrolled");
+    $(".nav-bar").removeClass("nav-bar-scrolled");
+  }
+}
